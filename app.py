df8fb51860c102b7c0e788ae255c121565f5fe03
Ankit Nainwal <78251168+nano-bot01@users.noreply.github.com>
2023-07-21 01:47:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,9 +33,31 @@ def transform_text(text):
 tfidf = pickle.load(open('model/vectorizer.pkl', 'rb'))
 model = pickle.load(open('model/model.pkl', 'rb'))
 
-st.title("SMS Spam Classifier")
+# Add css to make text bigger
+st.markdown(
+    """
+    <style>
+    textarea {
+        font-size: 1rem !important;
+    }
+    title {
+        font-size: 2rem !important;
+    }
+    header{
+        font-size: 3rem !important;
+        font-color : red;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
 
-input_data = st.text_input("Enter the message to check! ")
+st.markdown("<h1 style='text-align: center; color: red;'>SMS Spam Classifier</h1>", unsafe_allow_html=True)
+st.text("\n\n")
+
+
+st.markdown("<h5 style=' color: white;'>Enter the message to check! </h1>", unsafe_allow_html=True)
+input_data = st.text_area('')
 
 if st.button('Predict'):
     # preprocessing
@@ -49,7 +71,23 @@ if st.button('Predict'):
 
     # display
     if result == 1:
-        st.header("Spam")
+        st.header(":red[Spam Detected]")
     else:
-        st.header("Not Spam")
+        st.header(":green[Not Spam]")
+
+st.markdown("******")
+st.markdown("""
+        1. Sample Spam Message :   
+                You could be entitled up to E3,16Ø in compensation from mis-sold PPI on a credit card or Loan. Please reply for info or STOP to opt out.
+                
+        2. Sample Ham Message : 
+                Hii how are you!!!""")
+
+
+
+st.markdown("******")
+st.write(
+    "Contributor : [Ankit Nainwal](https://github.com/nano-bot01) \n [LinkedIn](https://www.linkedin.com/in/ankit-nainwal1/) ")
+
+st.write("\n© 2023 [SMS Spam Classifier using Machine Learning](https://github.com/nano-bot01/SMS-Spam-Classifier-Web-App-using-Machine-Learning).")
 
